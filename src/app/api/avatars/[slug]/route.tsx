cef7827a76db3625861c2fbb17383df4ea36228b
Michael K. Steinberg <m.kuper.steinberg@gmail.com>
2026-04-09 20:52:50 +0300

    ARGs in docker build

--- src/app/api/avatars/[slug]/route.tsx
+++ src/app/api/avatars/[slug]/route.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 import { AuthSessionData } from "@/api-shared/session";
